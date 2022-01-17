3736ef9a80d968155630a3b5e64a559b24283fb2
Olga Senakosava <senakosava.olga@gmail.com>
2022-01-17 16:46:10 +0100

    A huge change
    - added burger menu
    
    - made a mobile version

--- js/index.js
+++ js/index.js
@@ -1,8 +1,22 @@
-function openMenu() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
+
+
+
+  document.getElementById("burger").onclick = function() {openMenu()};
+
+  function openMenu() {
+    document.getElementById("menu-id").classList.toggle("show");
+  }
+
+
+  window.onclick = function(event) {
+  if (!event.target.matches('.burger')) {
+    let dropdowns = document.getElementsByClassName("menu-div");
+    let i;
+    for (i = 0; i < dropdowns.length; i++) {
+      let openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
   }
 }
